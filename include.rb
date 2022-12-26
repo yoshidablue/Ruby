f030cd826de6bd00ddb86cca0aa9f0d4834d0062
yoshidablue <sugar.blue.1224@gmail.com>
2022-12-26 04:37:56 +0000

    メソッド

---
+++ include.rb
@@ -0,0 +1,6 @@
+# 引数に指定した文字列が含まれているかどうかを調べるinclude?メソッド。
+
+puts "WEBCAMPでプログラミング学習".include?("WEBCAMP")
+  # 「WEBCAMPでプログラミング学習」の中には「WEBCAMP」が含まれているため、
+  # 正しいことを表すtrueが出力。含まれていない場合はfalseが出力される。
+  # 実行結果　true
---
+++ reverse.rb
@@ -0,0 +1,4 @@
+# 文字列を逆に表示するreverseメソッド。
+
+puts "WEBCAMP".reverse
+  # 実行結果　PMACBEW
